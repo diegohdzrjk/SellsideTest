b540dd74986676d24e7a1560f7ae3bb370a4395b
DiegoHernandez <diego.hernandez@hexagondata.io>
2022-11-18 22:41:04 +0100

    logging

--- SellSideapp.py
+++ SellSideapp.py
@@ -4,7 +4,7 @@ import Upload_Lineitems_2022_10_18 as uploadLI
 import time
 import logging
 
-logging.getLogger('googleapicliet.discovery_cache').setLevel(logging.ERROR)
+logging.getLogger('googleapicliet.locked_file').setLevel(logging.ERROR)
 
 st.sidebar.header('Input')
 #st.image("")
